2aa2173ac600c1502d01136ba5e16ef8058b35a4
virendra rajput <virendra.rajput567@gmail.com>
2013-10-07 00:53:49 +0530

    Codechef Oct long prob in progress

---
+++ Codechef/Kamehameha.py
@@ -0,0 +1,25 @@
+def main():
+    """
+    t = int(raw_input())
+    arr = []
+
+    while t > 0:
+        st = int(raw_input())
+        while st > 0:
+            b = raw_input()
+            arr.append([[int(i) for i in b.split(" ")]])
+            st -= 1
+        t -= 1
+    """
+    arr = [[[0, 0], [0, 1], [1, 0]]]
+
+    for grid in arr:
+        m = max(max(grid)) + 1
+        Mat = [[0 for x in xrange(m)] for x in xrange(m)]
+
+        for d in grid:
+           Mat[d[0]][d[1]] = 1
+
+        print Mat
+
+main()
